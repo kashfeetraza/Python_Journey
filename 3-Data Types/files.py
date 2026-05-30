6f15a50f953c9c2e51bfcbaec4323a3aebdd834e
Kashfeet Raza <kashfeetraza@gmail.com>
2026-05-30 23:27:24 +0500

    Add files via upload

---
+++ 3-Data Types/files.py
@@ -0,0 +1,39 @@
+# Interger
+a = 1
+# print(type(a))
+
+
+# Float 
+
+floating_Number = 10.5
+# print(type(floating_Number))
+
+# Complex Value
+
+Complex_Value = 23j
+# print(type(Complex_Value))
+
+# String Variables
+
+Name = "Ali Raza"
+Comam = '123456'
+# print(type(Name))
+# print(type(Comam))
+
+
+# Boolean Datatype
+
+isAvaliable = True
+# print(type(isAvaliable))
+
+# UNI Code 
+UniCodeOfA = 'A'
+# print(ord(UniCodeOfA))
+
+# convert uni code to char
+UniCodeConversion = 65
+# print(chr(UniCodeConversion))
+
+
+
+
