aeec091ab973552aa40bb5e6e65eac19108189f6
Kashfeet Raza <kashfeetraza@gmail.com>
2026-05-30 23:27:06 +0500

    Add files via upload

---
+++ 2-Commits and Variables/P1.py
@@ -0,0 +1,24 @@
+# Hello there i am kashfeet raza i want to say you something about
+# We use hashtag for the commits for the understanding the commit in python
+
+
+"""HEllo there is multi-line comment for the understanding for analogy of showing words"""
+
+"""Hello there is the work for the showing understanding multilines comments"""
+
+
+kash = "Kashfeet Ra"
+
+"""Thing that are not allowed in Creating Variable"""
+# 1-not allow for starting with Number
+# 1kash = "Hello"
+# 2-Not use spacing in Variable
+# kash raza = "ali"
+# 3-you should not uses Special characters in variable
+# KA@ash = "lili"
+
+KashfeetSchool = "Ali"  # Pascal Case
+
+kashfeetSchool = "Raza" # Camel Case
+
+kashfeet_School = "Yes" # Snake Case
