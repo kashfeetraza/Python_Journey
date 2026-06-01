c09d236aa78c9afe11495b32d8b1eea9c4a41853
Kashfeet Raza <kashfeetraza@gmail.com>
2026-06-01 21:01:18 +0500

    Add files via upload

---
+++ 6-Input-Output/prog.py
@@ -0,0 +1,13 @@
+# Print is output function
+print("Kashfeet")
+
+name = "lala"
+age = 90
+
+print("My name is",name,"& My age is",age)
+
+print(f"my name is {name} & my age is {age}")
+
+
+MyAge = int(input("What is your age: "))
+print("Yor age is:",MyAge)
