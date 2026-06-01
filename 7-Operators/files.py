0d77f5cf9fa34ecff9b073ccbd13fd5e42231114
Kashfeet Raza <kashfeetraza@gmail.com>
2026-06-01 21:01:39 +0500

    Add files via upload

---
+++ 7-Operators/files.py
@@ -0,0 +1,46 @@
+
+# Arithmetic Operations
+# a = 32
+# b = 10
+
+# print("a+b=",a+b)
+# print("a-b=",a-b)
+# print("a*b=",a*b)
+# print("b/a=",b/a)
+# print("b//a=",b//a)
+# print("b**a=",4**2)
+# print("a%b=",a%b)
+
+# Python understand the rule of BODMAS 
+
+# Assignment Operation
+
+# a = 24
+
+# Compound Assignment Operator
+# a = 20
+# a += 10
+# a += 20
+# a += 30
+# print("Addition of some values in a:",a)
+
+
+# Comparison Operation 
+"""
+>
+<
+>=
+<=
+==
+!=
+"""
+
+a = 21
+b = 21
+
+print(a != b)
+print(a == b)
+print(a >= b)
+print(a <= b)
+print(a < b)
+print(a > b)
